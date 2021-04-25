1e898f88b1a12622394f8a84feab02bdb8bfa6ca
GawlikP <rudepeeter@gmail.com>
2021-04-25 11:35:50 -0400

    Stworzono autoryzacje wraz z autentykacja. Szyfrowane haslo i zapisywanie sesji uzytkownika.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'hello#index'
+  #get '/signup', to: 'users#new'
+  #post '/signup', to: 'user#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
+  resources :users
 end
